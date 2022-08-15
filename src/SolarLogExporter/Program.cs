c08cdf62574313ebb4693bc6e7fa4a8ee2246b67
Waishon <mail@sbusse.de>
2022-08-16 01:43:22 +0200

    Add systemd support

--- src/SolarLogExporter/Program.cs
+++ src/SolarLogExporter/Program.cs
@@ -10,6 +10,9 @@ public class Program
         CreateHostBuilder(args).Build().Run();
     }
 
-    private static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
-        .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    private static IHostBuilder CreateHostBuilder(string[] args)
+        => Host.CreateDefaultBuilder(args).UseSystemd().ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.UseStartup<Startup>();
+        });
 }
